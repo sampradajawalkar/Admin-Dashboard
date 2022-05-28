c622cc794d93971a28d44c230114ebda4cefe13a
Samprada Jawalkar <78810033+sampradajawalkar@users.noreply.github.com>
2022-05-28 23:35:06 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+    basename: '',
+    defaultPath: '/dashboard/default',
+    fontFamily: `'Roboto', sans-serif`,
+    borderRadius: 12,
+    API_SERVER: 'http://localhost:5000/api/'
+};
+
+export default config;
---
+++ src/layout/MinimalLayout/index.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+//-----------------------|| MINIMAL LAYOUT ||-----------------------//
+
+const MinimalLayout = (props) => {
+    return (
+        <React.Fragment>
+            {props.children}
+        </React.Fragment>
+    );
+};
+
+MinimalLayout.propTypes = {
+    children: PropTypes.node
+};
+
+export default MinimalLayout;
